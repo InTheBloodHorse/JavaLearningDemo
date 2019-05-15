409875a39d82c9bb529cedb487c883181773db11
BestOfDp <2495088064@qq.com>
2019-05-15 12:11:58 +0800

    thread

---
+++ src/inthebloodhorse/thread/StopThread.java
@@ -0,0 +1,20 @@
+package inthebloodhorse.thread;
+
+
+public class StopThread {
+    private static boolean flag = true;
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(() -> {
+            int i = 0;
+            while (flag) {
+                i++;
+                System.out.println(i);
+            }
+        });
+        thread.start();
+        Thread.sleep(1000);
+//        TimeUnit.SECONDS.sleep(1);
+        flag = false;
+    }
+}
